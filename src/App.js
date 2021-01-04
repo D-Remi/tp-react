3f05e9c958cef78eb2813029785bbd9ad1551dc8
D-Remi <delfosseremi@hotmail.com>
2021-01-04 12:27:05 +0100

    troisieme commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-import Home from './component/home'
-
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+ 
+import Login from './component/login';
+import Home from './component/home';
+ 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token only)</small>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
-
+ 
 export default App;
