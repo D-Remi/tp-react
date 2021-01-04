95fcf75f00d607149a46e029b1aed3c59e48ab1d
D-Remi <delfosseremi@hotmail.com>
2021-01-04 14:19:39 +0100

    quatrieme commite

--- src/component/login.js
+++ src/component/login.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
  
 function Login() {
   return (
     <div>
-      <h1>mE CONNECTER</h1>
+      <h1>me CONNECTER</h1>
+      <form>
+        <label>Votre Email</label>
+        <input type="text" placeholder="email" name="email"/>
+        <label>Votre mot de passe</label>
+        <input type="password" placeholder="mot de passe" name="password"/>
+        <button type="submit" onclick="connect()">Se connecter</button>
+      </form>
     </div>
   );
+
+  function connect() {
+    //
+  }
 }
 
 export default Login;
